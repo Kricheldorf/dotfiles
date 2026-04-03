88d6dab071beebd794a102a28f75e8339695f16e
Guilherme Kricheldorf <gui.kricheldorf@gmail.com>
2026-04-02 21:33:34 -0300

    feat: add raw aconfmgr snapshot of current system

---
+++ aconfmgr/10-ignores.sh
@@ -0,0 +1,164 @@
+IgnorePath '/usr/*' # system files
+IgnorePath '/var/*' # system files
+IgnorePath '/etc/*' # system files
+IgnorePath '/boot/*' # system files
+IgnorePath '/opt/*' # system files
+IgnorePath '/.snapshots/*' # system files
+
+# CachyOS base files
+
+IgnorePackage accountsservice # D-Bus interface for user account query and manipulation
+IgnorePackage alsa-firmware # Firmware binaries for loader programs in alsa-tools and hotplug firmware loader
+IgnorePackage alsa-plugins # Additional ALSA plugins
+IgnorePackage alsa-utils # Advanced Linux Sound Architecture - Utilities
+IgnorePackage amd-ucode # Microcode update image for AMD CPUs
+IgnorePackage base # Minimal package set to define a basic Arch Linux installation
+IgnorePackage base-devel # Basic tools to build Arch Linux packages
+IgnorePackage bash-completion # Programmable completion for the bash shell
+IgnorePackage bind # A complete, highly portable implementation of the DNS protocol
+IgnorePackage btrfs-assistant # An application for managing BTRFS subvolumes and Snapper snapshots
+IgnorePackage btrfs-progs # Btrfs filesystem utilities
+IgnorePackage cachyos-fish-config # Fish configuration of CachyOS
+IgnorePackage cachyos-grub-theme # Grub2 Theme for CachyOS
+IgnorePackage cachyos-hello # Welcome screen for CachyOS
+IgnorePackage cachyos-hooks # CachyOS libalpm hooks
+IgnorePackage cachyos-kernel-manager # Simple kernel manager
+IgnorePackage cachyos-keyring # CachyOS keyring
+IgnorePackage cachyos-micro-settings # CachyOS micro settings
+IgnorePackage cachyos-mirrorlist # CachyOS repository mirrorlist
+IgnorePackage cachyos-packageinstaller # Simple Software Application Package Installer for CachyOS which helps setting up & installing applications
+IgnorePackage cachyos-plymouth-bootanimation # CachyOS Bootanimation (new)
+IgnorePackage cachyos-rate-mirrors # CachyOS - Rate mirrors service
+IgnorePackage cachyos-settings # CachyOS - Settings
+IgnorePackage cachyos-v3-mirrorlist # CachyOS repository mirrorlist
+IgnorePackage cachyos-v4-mirrorlist # CachyOS repository mirrorlist
+IgnorePackage cachyos-zsh-config # Zsh configuration for CachyOS
+IgnorePackage cantarell-fonts # Humanist sans serif font
+IgnorePackage chwd # CachyOS Hardware Detection Tool
+IgnorePackage cpupower # Linux kernel tool to examine and tune power saving related features of your processor
+IgnorePackage cryptsetup # Userspace setup tool for transparent encryption of block devices using dm-crypt
+IgnorePackage device-mapper # Device mapper userspace library and tools
+IgnorePackage dhclient # A standalone DHCP client from the dhcp package
+IgnorePackage dmidecode # Desktop Management Interface table related utilities
+IgnorePackage dmraid # Device mapper RAID interface
+IgnorePackage dnsmasq # Lightweight, easy to configure DNS forwarder and DHCP server
+IgnorePackage dosfstools # DOS filesystem utilities
+IgnorePackage duf # Disk Usage/Free Utility
+IgnorePackage e2fsprogs # Ext2/3/4 filesystem utilities
+IgnorePackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
+IgnorePackage efitools # Tools for manipulating UEFI secure boot platforms
+IgnorePackage egl-wayland # EGLStream-based Wayland external platform
+IgnorePackage ethtool # Utility for controlling network drivers and hardware
+IgnorePackage ex-vi-compat # The ex and vi commands based on vim's compatibility modes
+IgnorePackage exfatprogs # exFAT filesystem userspace utilities for the Linux Kernel exfat driver
+IgnorePackage f2fs-tools # Tools for Flash-Friendly File System (F2FS)
+IgnorePackage fastfetch # A feature-rich and performance oriented neofetch like system information tool
+IgnorePackage ffmpegthumbnailer # Lightweight video thumbnailer that can be used by file managers
+IgnorePackage firefox # Fast, Private & Safe Web Browser
+IgnorePackage fsarchiver # Safe and flexible file-system backup and deployment tool
+IgnorePackage fuse2 # Interface for userspace programs to export a filesystem to the Linux kernel
+IgnorePackage grub # GNU GRand Unified Bootloader (2)
+IgnorePackage grub-btrfs-support # Support package for enabling grub-btrfs support
+IgnorePackage grub-hook # Pacman hook to update GRUB after a kernel update
+IgnorePackage gst-libav # Multimedia graph framework - libav plugin
+IgnorePackage gst-plugin-pipewire # Multimedia graph framework - pipewire plugin
+IgnorePackage gst-plugins-bad # Multimedia graph framework - bad plugins
+IgnorePackage gst-plugins-ugly # Multimedia graph framework - ugly plugins
+IgnorePackage haveged # Entropy harvesting daemon using CPU timings
+IgnorePackage hdparm # A shell utility for manipulating Linux IDE drive/driver parameters
+IgnorePackage hwdetect # Hardware detection script with loading modules and mkinitcpio.conf
+IgnorePackage hwinfo # Hardware detection tool from openSUSE
+IgnorePackage inetutils # A collection of common network programs
+IgnorePackage iptables-nft # Linux kernel packet control tool (using nft interface)
+IgnorePackage iwd # Internet Wireless Daemon
+IgnorePackage jfsutils # JFS filesystem utilities
+IgnorePackage less # A terminal based program for viewing text files
+IgnorePackage lib32-nvidia-utils # NVIDIA drivers utilities (32-bit)
+IgnorePackage lib32-opencl-nvidia # OpenCL implemention for NVIDIA (32-bit)
+IgnorePackage lib32-vulkan-icd-loader # Vulkan Installable Client Driver (ICD) Loader (32-bit)
+IgnorePackage libdvdcss # Portable abstraction library for DVD decryption
+IgnorePackage libgsf # Extensible I/O abstraction library for dealing with structured file formats
+IgnorePackage libopenraw # Library for decoding RAW files
+IgnorePackage libva-nvidia-driver # VA-API implementation that uses NVDEC as a backend
+IgnorePackage libwnck3 # Library to manage X windows and workspaces (for e.g. pagers or tasklists)
+IgnorePackage linux-cachyos # The Linux EEVDF + LTO + AutoFDO + Propeller Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel and modules
+IgnorePackage linux-cachyos-headers # Headers and scripts for building modules for the Linux EEVDF + LTO + AutoFDO + Propeller Cachy Sauce Kernel by CachyOS with other patches and improvements. kernel
+IgnorePackage linux-cachyos-lts # The Linux BORE + Cachy Sauce Kernel by CachyOS with other patches and improvements - Long Term Service kernel and modules
+IgnorePackage linux-cachyos-lts-headers # Headers and scripts for building modules for the Linux BORE + Cachy Sauce Kernel by CachyOS with other patches and improvements - Long Term Service kernel
+IgnorePackage linux-cachyos-lts-nvidia-open # nvidia open modules of 595.58.03 driver for the linux-cachyos-lts kernel
+IgnorePackage linux-cachyos-nvidia-open # nvidia open modules of 595.58.03 driver for the linux-cachyos kernel
+IgnorePackage linux-firmware # Firmware files for Linux - Default set
+IgnorePackage logrotate # Rotates system logs automatically
+IgnorePackage lsb-release # LSB version query program
+IgnorePackage lsscsi # A tool that lists devices connected via SCSI and its transports
+IgnorePackage lvm2 # Logical Volume Manager 2 utilities
+IgnorePackage man-db # A utility for reading man pages
+IgnorePackage man-pages # Linux man pages
+IgnorePackage mdadm # A tool for managing/monitoring Linux md device arrays, also known as Software RAID
+IgnorePackage meld # Compare files, directories and working copies
+IgnorePackage mesa-utils # Essential Mesa utilities
+IgnorePackage micro # Modern and intuitive terminal-based text editor
+IgnorePackage mkinitcpio # Modular initramfs image creation utility
+IgnorePackage modemmanager # Mobile broadband modem management service
+IgnorePackage mtools # A collection of utilities to access MS-DOS disks
+IgnorePackage nano # Pico editor clone with enhancements
+IgnorePackage nano-syntax-highlighting # Nano editor syntax highlighting enhancements
+IgnorePackage nfs-utils # Support programs for Network File Systems
+IgnorePackage nilfs-utils # A log-structured file system supporting continuous snapshotting (userspace utils)
+IgnorePackage nss-mdns # glibc plugin providing host name resolution via mDNS
+IgnorePackage ntp # Network Time Protocol reference implementation
+IgnorePackage nvidia-settings # Tool for configuring the NVIDIA graphics driver
+IgnorePackage nvidia-utils # NVIDIA drivers utilities
+IgnorePackage octopi # A powerful Pacman frontend using Qt libs
+IgnorePackage opencl-nvidia # OpenCL implemention for NVIDIA
+IgnorePackage openssh # SSH protocol implementation for remote login, command execution and file transfer
+IgnorePackage os-prober # Utility to detect other OSes on a set of drives
+IgnorePackage pacman-contrib # Contributed scripts and tools for pacman systems
+IgnorePackage paru # Feature packed AUR helper
+IgnorePackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
+IgnorePackage pipewire-pulse # Low-latency audio/video router and processor - PulseAudio replacement
+IgnorePackage pkgfile # A tool to search for files in official repository packages
+IgnorePackage poppler-glib # Poppler glib bindings
+IgnorePackage power-profiles-daemon # Makes power profiles handling available over D-Bus
+IgnorePackage pv # A terminal-based tool for monitoring the progress of data through a pipeline
+IgnorePackage python # The Python programming language
+IgnorePackage python-defusedxml # XML bomb protection for Python stdlib modules
+IgnorePackage python-packaging # Core utilities for Python packages
+IgnorePackage rebuild-detector # Detects which packages need to be rebuilt
+IgnorePackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
+IgnorePackage resvg # SVG rendering library and CLI
+IgnorePackage rsync # A fast and versatile file copying tool for remote and local files
+IgnorePackage rtkit # Realtime Policy and Watchdog Daemon
+IgnorePackage s-nail # Environment for sending and receiving mail
+IgnorePackage sddm # QML based X11 and Wayland display manager
+IgnorePackage sg3_utils # Generic SCSI utilities
+IgnorePackage smartmontools # Control and monitor S.M.A.R.T. enabled ATA and SCSI Hard Drives
+IgnorePackage snapper # A tool for managing BTRFS and LVM snapshots
+IgnorePackage sof-firmware # Sound Open Firmware
+IgnorePackage starship # The cross-shell prompt for astronauts
+IgnorePackage sudo # Give certain users the ability to run some commands as root
+IgnorePackage sway # Tiling Wayland compositor and replacement for the i3 window manager
+IgnorePackage swaybg # Wallpaper tool for Wayland compositors
+IgnorePackage sysfsutils # System Utilities Based on Sysfs
+IgnorePackage texinfo # GNU documentation system for on-line information and printed output
+IgnorePackage ufw # Uncomplicated and easy to use CLI tool for managing a netfilter firewall
+IgnorePackage upower # Abstraction for enumerating power devices, listening to device events and querying history and statistics
+IgnorePackage usb_modeswitch # Activating switchable USB devices on Linux.
+IgnorePackage usbutils # A collection of USB tools to query connected USB devices
+IgnorePackage uwsm # A standalone Wayland session manager
+IgnorePackage vulkan-icd-loader # Vulkan Installable Client Driver (ICD) Loader
+IgnorePackage wget # Network utility to retrieve files from the web
+IgnorePackage which # A utility to show the full path of commands
+IgnorePackage wireless-regdb # Central Regulatory Domain Database
+IgnorePackage wireplumber # Session / policy manager implementation for PipeWire
+IgnorePackage wpa_supplicant # A utility providing key negotiation for WPA wireless networks
+IgnorePackage xf86-input-libinput # Generic input driver for the X.Org server based on libinput
+IgnorePackage xfsprogs # XFS filesystem utilities
+IgnorePackage xl2tpd # an open source implementation of the L2TP maintained by Xelerance Corporation
+IgnorePackage xorg-server # Xorg X server
+IgnorePackage xorg-xdpyinfo # Display information utility for X
+IgnorePackage xorg-xinit # X.Org initialisation program
+IgnorePackage xorg-xinput # Small commandline tool to configure devices
+IgnorePackage xorg-xkill # Kill a client by its X resource
+IgnorePackage xorg-xrandr # Primitive command line interface to RandR extension
+IgnorePackage zsh-autocomplete # Real-time type-ahead completion for Zsh
---
+++ aconfmgr/99-unsorted.sh
@@ -0,0 +1,132 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# AUR packages
+
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+AddPackage --foreign ente-auth-bin # Ente two-factor authenticator.
+AddPackage --foreign insync # An unofficial Dropbox, Google Drive, and OneDrive client that runs on Linux, with support for various desktops
+AddPackage --foreign slack-desktop # Slack Desktop (Beta) for Linux
+AddPackage --foreign tmux-plugin-manager # tpm - Tmux Plugin Manager
+AddPackage --foreign ttf-ms-fonts # Core TTF Fonts from Microsoft
+AddPackage --foreign vial-appimage # Vial is an open-source cross-platform (Windows, Linux and Mac) GUI and a QMK fork for configuring your keyboard in real time, similar to VIA.
+AddPackage --foreign vicinae-bin # Raycast like FOSS app on Linux
+
+# Packages to install
+
+AddPackage 7zip # File archiver for extremely high compression
+AddPackage awesome-terminal-fonts # fonts/icons for powerlines
+AddPackage aws-cli-v2 # Universal Command Line Interface for Amazon Web Services (version 2)
+AddPackage bat-extras # Bash scripts that integrate bat with various command line tools
+AddPackage bats # Bash Automated Testing System
+AddPackage bitwarden # A secure and free password manager for all of your devices
+AddPackage bluetui # TUI for managing bluetooth devices
+AddPackage bluez # Daemons for the bluetooth protocol stack
+AddPackage bluez-hid2hci # Put HID proxying bluetooth HCI's into HCI mode
+AddPackage bluez-libs # Deprecated libraries for the bluetooth protocol stack
+AddPackage bluez-utils # Development and debugging utilities for the bluetooth protocol stack
+AddPackage brave-bin # Web browser that blocks ads and trackers by default (binary release)
+AddPackage btop # A monitor of system resources, bpytop ported to C++
+AddPackage csvlens # Command line csv viewer
+AddPackage dbeaver # Free universal SQL Client for developers and database administrators (community edition)
+AddPackage diffnav # git diff pager based on delta but with a file tree, à la GitHub
+AddPackage diffutils # Utility programs used for creating patch files
+AddPackage docker # Pack, ship and run any application as a lightweight container
+AddPackage docker-compose # Fast, isolated development environments using Docker
+AddPackage dolphin # KDE File Manager
+AddPackage dunst # Customizable and lightweight notification-daemon
+AddPackage fnm # Fast and simple Node.js version manager, built with Rust
+AddPackage git # the fast distributed version control system
+AddPackage git-delta # Syntax-highlighting pager for git and diff output
+AddPackage github-cli # The GitHub CLI
+AddPackage gnome-keyring # Stores passwords and encryption keys
+AddPackage go # Core compiler tools for the Go programming language
+AddPackage hledger # Command-line interface for the hledger accounting system
+AddPackage hledger-ui # Curses-style terminal interface for the hledger accounting system
+AddPackage hledger-web # Web-based user interface for the hledger accounting system
+AddPackage hyprland # a highly customizable dynamic tiling Wayland compositor
+AddPackage hyprlock # hyprland’s GPU-accelerated screen locking utility
+AddPackage hyprpicker # A wlroots-compatible Wayland color picker that does not suck
+AddPackage hyprpolkitagent # Simple polkit authentication agent for Hyprland, written in QT/QML
+AddPackage hyprshot # Hyprland screenshot utility
+AddPackage keyd # A key remapping daemon for linux
+AddPackage kitty # A modern, hackable, featureful, OpenGL-based terminal emulator
+AddPackage lazydocker # A simple terminal UI for docker and docker-compose, written in Go with the gocui library.
+AddPackage lazygit # Simple terminal UI for git commands
+AddPackage libappindicator # Allow applications to extend a menu via Ayatana indicators in Unity, KDE or Systray
+AddPackage localsend # An open source cross-platform alternative to AirDrop
+AddPackage ly # TUI display manager
+AddPackage ncdu # Disk usage analyzer with an ncurses interface
+AddPackage neovim-git # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage netctl # Profile based systemd network management
+AddPackage network-manager-applet # Applet for managing network connections
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage networkmanager-openvpn # NetworkManager VPN plugin for OpenVPN (with GUI)
+AddPackage noto-color-emoji-fontconfig # Fontconfig to enable Noto Color Emoji fonts where emojis can be displayed
+AddPackage noto-fonts # Google Noto TTF fonts
+AddPackage noto-fonts-cjk # Google Noto CJK fonts
+AddPackage noto-fonts-emoji # Google Noto Color Emoji font
+AddPackage nwg-look # GTK settings editor adapted to work on wlroots-based compositors
+AddPackage obsidian # A powerful knowledge base that works on top of a local folder of plain text Markdown files
+AddPackage okular # Document Viewer
+AddPackage onlyoffice-bin # An office suite that combines text, spreadsheet and presentation editors
+AddPackage pavucontrol # PulseAudio Volume Control
+AddPackage perl # A highly capable, feature-rich programming language
+AddPackage perl-image-exiftool # Read and write EXIF information
+AddPackage playerctl # mpris media player controller and lib for spotify, vlc, audacious, bmp, xmms2, and others.
+AddPackage plocate # Alternative to locate, faster and compatible with mlocate's database.
+AddPackage plymouth # Graphical boot splash screen
+AddPackage polkit-kde-agent # Daemon providing a polkit authentication UI for KDE
+AddPackage postgresql # Sophisticated object-relational DBMS
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage satty # Modern screenshot annotation tool, inspired by Swappy and Flameshot
+AddPackage seahorse # GNOME application for managing PGP keys
+AddPackage spotify-player # A command driven spotify player
+AddPackage stow # Manage installation of multiple softwares in the same directory tree
+AddPackage syncthing # Open Source Continuous Replication / Cluster Synchronization Thing
+AddPackage tmux # Terminal multiplexer
+AddPackage ttf-bitstream-vera # Bitstream Vera fonts.
+AddPackage ttf-dejavu # Font family based on the Bitstream Vera Fonts with a wider range of characters
+AddPackage ttf-liberation # Font family which aims at metric compatibility with Arial, Times New Roman, and Courier New
+AddPackage ttf-meslo-nerd # Patched font Meslo LG from nerd fonts library
+AddPackage ttf-opensans # Sans-serif typeface commissioned by Google
+AddPackage unrar # The RAR uncompression program
+AddPackage unzip # For extracting and viewing files in .zip archives
+AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
+AddPackage waybar # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage wayland-protocols # Specifications of extended Wayland protocols
+AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
+AddPackage wmenu # Efficient dynamic menu for Wayland and wlroots based Wayland compositors
+AddPackage wofi # launcher for wlroots-based wayland compositors
+AddPackage xdg-desktop-portal # Desktop integration portals for sandboxed apps
+AddPackage xdg-desktop-portal-hyprland # xdg-desktop-portal backend for hyprland
+AddPackage xdg-desktop-portal-wlr # xdg-desktop-portal backend for wlroots
+AddPackage xdg-user-dirs # Manage user directories like ~/Desktop and ~/Music
+AddPackage xorg-xwayland # run X clients under wayland
+AddPackage yazi # Blazing fast terminal file manager written in Rust, based on async I/O
+AddPackage zoxide # A smarter cd command for your terminal
+
+IgnorePackage --foreign gitflow-cjs # CJS Edition of the git extensions to provide high-level repository operations for Vincent Driessen's branching model.
+
+RemovePackage --foreign timer-bin # Timer is like sleep, but reports progress.
+RemovePackage asciinema # Record and share terminal sessions
+RemovePackage code # The Open Source build of Visual Studio Code (vscode) editor
+RemovePackage ghostty # Fast, native, feature-rich terminal emulator pushing modern features
+RemovePackage glances # CLI curses-based monitoring tool
+RemovePackage gradle # Powerful build system for the JVM
+RemovePackage gum # A tool for glamorous shell scripts
+RemovePackage nushell # A new type of shell
+RemovePackage pnpm # Fast, disk space efficient package manager
+RemovePackage television # A general purpose fuzzy finder for your terminal
